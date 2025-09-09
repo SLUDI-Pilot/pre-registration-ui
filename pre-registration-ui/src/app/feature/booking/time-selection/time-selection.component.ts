e3cdb2956ff2033990f0db8ec05478afcc0c96ab
Vishal05-dev <vishalvenugopal00@gmail.com>
2025-09-09 20:41:15 +0530

    Revert "Changes made to display the full name on the Acknowledgement page"
    This reverts commit 29b9a891543e14ccb7fa861eec65be3295393b51.

--- pre-registration-ui/src/app/feature/booking/time-selection/time-selection.component.ts
+++ pre-registration-ui/src/app/feature/booking/time-selection/time-selection.component.ts
@@ -189,17 +189,13 @@ export class TimeSelectionComponent
       let filteredLangs = applicationLanguages.filter(applicationLang => 
         applicationLang == this.userPreferredLangCode
       );
-      let fullNameConcat = "";
       if (filteredLangs.length > 0) {
-        for (var names of this.name.split(",")) {
-          let nameValues = demographicData[names] == null ? [] : demographicData[names];
-          nameValues.forEach(nameVal => {
-            if (nameVal["language"] == this.userPreferredLangCode) {
-              fullNameConcat += nameVal["value"] + " ";
-            }
-          });
-        }
-        nameList.fullName = fullNameConcat;
+        let nameValues = demographicData[this.name];
+        nameValues.forEach(nameVal => {
+          if (nameVal["language"] == this.userPreferredLangCode) {
+            nameList.fullName = nameVal["value"];
+          }
+        });  
       } else {
         nameList.fullName =
         demographicData[this.name][0].value;
